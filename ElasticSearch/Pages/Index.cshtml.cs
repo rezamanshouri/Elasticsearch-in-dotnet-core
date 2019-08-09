6f628daeb9cbab16c654e87e96022eff0292fd09
rmanshouri <reza.manshouri@factset.com>
2019-08-09 16:34:30 -0400

    Add project files.

---
+++ ElasticSearch/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ElasticSearch.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

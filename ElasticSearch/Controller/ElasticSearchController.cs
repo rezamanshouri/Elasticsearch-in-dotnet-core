d63bbd415630a63cd80280fb5ae8096c0c60327a
rmanshouri <reza.manshouri@factset.com>
2019-08-09 17:15:33 -0400

    Add two endpoints as a way to trigger index/serach

---
+++ ElasticSearch/Controller/ElasticSearchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticSearch.Controller
+{
+    [Route("/es")]
+    public class ElasticSearchController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [Route("index", Name = "Index")]
+        [HttpGet]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public IActionResult Index()
+        {
+            return Ok(true);
+        }
+
+        [Route("search/{query}", Name = "Search")]
+        [HttpGet]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult Search(string query)
+        {
+            // TODO: this should be a POST
+            return Ok(query);
+        }
+    }
+}
